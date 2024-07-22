6e0fedfd510194086ba4e09ee219a98d99a256f6
Corey Pyle <corepyle@amazon.com>
2024-07-22 12:16:35 -0400

    Error on missing entities, replace on empty entities.

--- aws_doc_sdk_examples_tools/entities_test.py
+++ aws_doc_sdk_examples_tools/entities_test.py
@@ -17,11 +17,10 @@ def test_entity_errors_append():
         errors.append("invalid item")
 
 
-def test_expand_all_entities():
+def test_expand_missing_entities():
     entity_map = {
         "&entity1;": "expanded1",
         "&entity2;": "expanded2",
-        "&entity3;": "",
     }
 
     text = "This is a text with &entity1; and &entity2; and &entity3;"
@@ -32,6 +31,18 @@ def test_expand_all_entities():
     assert isinstance(errors._errors[0], MissingEntityError)
     assert errors._errors[0].entity == "&entity3;"
 
+def test_expand_empty_entity():
+    entity_map = {
+        "&entity1;": "expanded1",
+        "&entity2;": "expanded2",
+        "&entity3;": ""
+    }
+
+    text = "This is a text with &entity1; and &entity2; and &entity3;"
+    expanded_text, errors = expand_all_entities(text, entity_map)
+
+    assert expanded_text == "This is a text with expanded1 and expanded2 and "
+    assert len(errors._errors) == 0
 
 def test_expand_all_entities_with_no_entities():
     entity_map = {
