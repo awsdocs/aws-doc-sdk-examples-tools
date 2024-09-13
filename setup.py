0d0f5d13c9b255bb0ccf6d4d6699642433407dca
Laren-AWS <57545972+Laren-AWS@users.noreply.github.com>
2024-09-13 11:00:09 -0700

    Update setup.py to latest tagged version. (#87)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="aws_doc_sdk_examples_tools",
-    version="1.0.4+2024-09-11-A",
+    version="1.0.4+2024-09-13-B",
     packages=["aws_doc_sdk_examples_tools"],
     package_data={"aws_doc_sdk_examples_tools": ["config/*.yaml"]},
     entry_points={
