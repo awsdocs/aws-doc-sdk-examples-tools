1ed1fde55700a23a4eef2b87abd655db6f480622
Laren-AWS <57545972+Laren-AWS@users.noreply.github.com>
2024-09-19 11:38:20 -0700

    Update release tag in setup.py. (#91)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="aws_doc_sdk_examples_tools",
-    version="1.0.4+2024-09-13-B",
+    version="1.0.4+2024-09-19-A",
     packages=["aws_doc_sdk_examples_tools"],
     package_data={"aws_doc_sdk_examples_tools": ["config/*.yaml"]},
     entry_points={
