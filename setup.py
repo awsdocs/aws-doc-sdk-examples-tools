9364e011b869a466fda9bc72874698d8b7f49804
David Souther <davidsouther+github@gmail.com>
2024-11-20 10:29:49 -0500

    Release 2024.47.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="aws_doc_sdk_examples_tools",
-    version="2024.47.0",
+    version="2024.47.1",
     packages=["aws_doc_sdk_examples_tools"],
     package_data={"aws_doc_sdk_examples_tools": ["config/*.yaml"]},
     entry_points={
