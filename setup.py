80ce317601815ecf7ca94226584845e555d2eb92
Laren-AWS <larencra@amazon.com>
2024-12-12 15:07:44 -0800

    Release 2024.50.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="aws_doc_sdk_examples_tools",
-    version="2024.50.1",
+    version="2024.50.2",
     packages=["aws_doc_sdk_examples_tools"],
     package_data={"aws_doc_sdk_examples_tools": ["config/*.yaml"]},
     entry_points={
