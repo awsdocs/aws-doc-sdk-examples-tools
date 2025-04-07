d5e2b687acc59cb583d8fd5fbdd691da82fe4fde
Laren Crawford <larencra@amazon.com>
2025-04-07 13:41:57 -0700

    Release 2025.14.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="aws_doc_sdk_examples_tools",
-    version="2025.13.0",
+    version="2025.14.0",
     packages=["aws_doc_sdk_examples_tools"],
     package_data={"aws_doc_sdk_examples_tools": ["config/*.yaml"]},
     entry_points={
