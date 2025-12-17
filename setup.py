e2ccf068b80e21013b0bef0b5564ae086af3c8cf
Corey Pyle <corepyle@amazon.com>
2025-12-17 11:16:29 -0500

    Release 2025.50.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="aws_doc_sdk_examples_tools",
-    version="2025.41.0",
+    version="2025.50.0",
     packages=["aws_doc_sdk_examples_tools"],
     package_data={"aws_doc_sdk_examples_tools": ["config/*.yaml"]},
     entry_points={
