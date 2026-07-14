4363228a991c16d7e7895d40624f33e3db693284
Anirudh Yellanki <syellank@amazon.com>
2026-07-14 18:18:43 +0000

    Release 2026.28.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="aws_doc_sdk_examples_tools",
-    version="2026.10.0",
+    version="2026.28.0",
     packages=["aws_doc_sdk_examples_tools"],
     package_data={"aws_doc_sdk_examples_tools": ["config/*.yaml"]},
     entry_points={
