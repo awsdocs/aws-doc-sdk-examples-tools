1f977bb6ebc8f8028cb00ce62fe0e008cee027be
Danning Zhang <danningz@amazon.com>
2026-09-18 10:38:39 -0700

    Release 2026.37.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="aws_doc_sdk_examples_tools",
-    version="2026.36.1",
+    version="2026.37.0",
     packages=["aws_doc_sdk_examples_tools"],
     package_data={"aws_doc_sdk_examples_tools": ["config/*.yaml"]},
     entry_points={
